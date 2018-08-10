7b3829ddb89dd3b237eff50ec42a0bf3a08006e8
김문섭 <kmsup2@gmail.com>
2018-08-10 22:49:06 +0900

    아직 하는중.. jsp 쩝...

---
+++ supstagram/src/main/java/net/sup22/SupstagramApplication.java
@@ -0,0 +1,12 @@
+package net.sup22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupstagramApplication.class, args);
+	}
+}
