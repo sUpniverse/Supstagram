7506ae84e54ff3ef096fdd4a42f8871fce5b46f5
김문섭 <kmsup2@gmail.com>
2018-08-25 23:32:38 +0900

    qna 게시판 리스트구현

--- supstagram/src/main/java/net/sup22/domain/Question.java
+++ supstagram/src/main/java/net/sup22/domain/Question.java
@@ -1,5 +1,7 @@
 package net.sup22.domain;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -24,7 +26,7 @@ public class Question{
 	
 	private String title;
 	private String contents;	
-	
+	private LocalDateTime createDate;
 	
 	public Question() {
 		// TODO Auto-generated constructor stub
@@ -34,8 +36,48 @@ public class Question{
 		super();
 		this.writer = writer;
 		this.title = title;
-		this.contents = contents;
-		
+		this.contents = contents;		
+		this.createDate = LocalDateTime.now();
 	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public User getWriter() {
+		return writer;
+	}
+
+	public void setWriter(User writer) {
+		this.writer = writer;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}			
 	
 }
--- supstagram/src/main/java/net/sup22/web/QuestionController.java
+++ supstagram/src/main/java/net/sup22/web/QuestionController.java
@@ -1,15 +1,34 @@
 package net.sup22.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.sup22.domain.Question;
+import net.sup22.domain.QuestionRepository;
 
 @Controller
 @RequestMapping("/qna")
 public class QuestionController {
 	
+	@Autowired
+	private QuestionRepository questionRepository;
+	
 	@GetMapping("")
-	public String Qna() {
+	public String Qna(Model model) {		
+		model.addAttribute("questions", questionRepository.findAll());		
 		return "/qna/board";
+	}
+	
+	@GetMapping("/{id}")
+	public String viewQuestion(@PathVariable Long id, Model model) {
+		Question question = questionRepository.findById(id).get();
+		model.addAttribute("question", question);
+		return "/qna/show";
 	}
 }
