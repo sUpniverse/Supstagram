a3f9e8eb82bde22873b1487716ab7396e094d6dc
김문섭 <kmsup2@gmail.com>
2018-09-10 23:07:57 +0900

    qna게시판 구현 및 스마트에디터 추가

--- supstagram/src/main/java/net/sup22/web/QuestionController.java
+++ supstagram/src/main/java/net/sup22/web/QuestionController.java
@@ -1,16 +1,18 @@
 package net.sup22.web;
 
-import java.util.List;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.sup22.domain.Question;
 import net.sup22.domain.QuestionRepository;
+import net.sup22.domain.User;
 
 @Controller
 @RequestMapping("/qna")
@@ -31,4 +33,28 @@ public class QuestionController {
 		model.addAttribute("question", question);
 		return "/qna/show";
 	}
+	
+	@GetMapping("/form")
+	public String QnaForm(HttpSession session) {
+		User user = (User)session.getAttribute("user");
+		if(user == null) {
+			return "redirect:/users/login";
+		}
+		return "/qna/form";
+	}
+	
+	@PostMapping("")
+	public String Create(String title, String contents, HttpSession session) {
+		User user = (User)session.getAttribute("user");
+		if(user == null) {
+			return "redirect:/user/login";
+		}
+		
+		if(title.isEmpty()) {
+			return "redirect:/qna/form";
+		}
+		Question question = new Question(user, title, contents);
+		questionRepository.save(question);
+		return "redirect:/qna";
+	}	
 }
